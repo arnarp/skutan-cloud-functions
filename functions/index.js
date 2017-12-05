b06655459346b30204115cb0ca4b8a124799d703
Arnar Páll Birgisson <arnarp@gmail.com>
2017-12-05 18:57:39 +0000

    Init

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var functions = require("firebase-functions");
+var admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+exports.onUserCreation = functions.auth.user().onCreate(function (event) {
+    var user = event.data;
+    var userObject = {
+        displayName: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        claims: {}
+    };
+    return admin.database().ref("/users/" + user.uid).set(userObject).then(function () {
+        return admin.firestore().collection('users').doc(user.uid).set(userObject);
+    });
+});
+exports.onClaimWrite = functions.firestore.document('users/{uid}').onWrite(function (event) {
+    var user = event.data.data();
+    console.log(user.claims);
+    return admin.auth().setCustomUserClaims(event.params.uid, user.claims);
+});
+//# sourceMappingURL=index.js.map
---
+++ functions/src/index.ts
@@ -0,0 +1,25 @@
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+admin.initializeApp(functions.config().firebase);
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+
+exports.onUserCreation = functions.auth.user().onCreate(event => {
+  const user = event.data
+  const userObject = {
+    displayName: user.displayName,
+    email: user.email,
+    photoURL: user.photoURL,
+    claims: {}
+  }
+  return admin.database().ref(`/users/${user.uid}`).set(userObject).then(() => {
+    return admin.firestore().collection('users').doc(user.uid).set(userObject)
+  })
+})
+
+exports.onClaimWrite = functions.firestore.document('users/{uid}').onWrite(event => {
+  const user = event.data.data()
+  console.log(user.claims)
+  return admin.auth().setCustomUserClaims(event.params.uid, user.claims)
+})
